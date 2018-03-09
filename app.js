7fff0499effd4a776d6322ce159b73f8c2c923c0
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-03-09 14:49:08 -0800

    index.html

--- app.js
+++ app.js
@@ -32,12 +32,18 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) { 
+  // multiply the numbers
+var multNum = a * b;
+  //concatanate the string
+var multString = 'The product of ' + a + ' and ' + b + ' is ' + multNum + '.';
+  //return an array
+return [multNum, multString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
